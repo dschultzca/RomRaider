087738d64a4fb9b5b56e25bd5a09b1e6cf546903
Dale Schultz <dschultzca@yahoo.ca>
2018-12-22 11:36:00 -0500

    Find Java and offer to launch Editor or Logger

--- run.bat
+++ run.bat
@@ -1,11 +1,43 @@
-:: Uncomment the line for the mode you wish to run.  Comment all other lines.
-:: Adjust path to javaw.exe as required for your Java system installation
-rem start javaw -Djava.library.path=lib/windows -Dawt.useSystemAAFontSettings=lcd -Dswing.aatext=true -Dsun.java2d.d3d=true -Xms64M -Xmx512M -jar RomRaider.jar 1>>"%HOMEPATH%\.RomRaider\romraider_sout.log" 2>&1
-:: Start Editor
-start javaw.exe -Djava.library.path=lib/windows -Dawt.useSystemAAFontSettings=lcd -Dswing.aatext=true -Dsun.java2d.d3d=true -Xms64M -Xmx512M -jar RomRaider.jar
-:: Start Logger in full screen mode
-rem start javaw -Djava.library.path=lib/windows -Dawt.useSystemAAFontSettings=lcd -Dswing.aatext=true -Dsun.java2d.d3d=true -Xms64M -Xmx512M -jar RomRaider.jar -logger.fullscreen
-:: Start Logger
-rem start javaw -Djava.library.path=lib/windows -Dawt.useSystemAAFontSettings=lcd -Dswing.aatext=true -Dsun.java2d.d3d=true -Xms64M -Xmx512M -jar RomRaider.jar -logger
-:: Start Logger in Touch screen mode
-rem start javaw -Djava.library.path=lib/windows -Dawt.useSystemAAFontSettings=lcd -Dswing.aatext=true -Dsun.java2d.d3d=true -Xms64M -Xmx512M -jar RomRaider.jar -logger.touch
+@echo off 
+    setlocal enableextensions disabledelayedexpansion
+
+    rem Where to find java information in registry
+    set "javaKey=HKLM\SOFTWARE\Wow6432Node\JavaSoft\Java Runtime Environment"
+
+    rem Get current java version
+    set "javaVersion="
+    for /f "tokens=3" %%v in ('reg query "%javaKey%" /v "CurrentVersion" 2^>nul') do set "javaVersion=%%v"
+
+    rem Test if a java version has been found
+    if not defined javaVersion (
+        echo Java version not found
+        goto endProcess
+    )
+
+    rem Get java home for current java version
+    set "javaDir="
+    for /f "tokens=2,*" %%d in ('reg query "%javaKey%\%javaVersion%" /v "JavaHome" 2^>nul') do set "javaDir=%%e\bin\javaw.exe"
+
+    if not defined javaDir (
+        echo Java directory not found
+    ) else (
+        echo Found Java %javaVersion% in : "%javaDir%"
+    )
+
+    choice /T 3 /D n /M "Start RomRaider Editor"
+    if errorlevel 255 goto endProcess
+    if errorlevel 2   goto logger
+    if errorlevel 1   goto editor
+    if errorlevel 0   goto endProcess
+
+:editor
+    echo starting **** RomRaider Editor ****
+    start "" "%javaDir%" -Djava.library.path=lib/windows -Dawt.useSystemAAFontSettings=lcd -Dswing.aatext=true -Dsun.java2d.d3d=true -Xms64M -Xmx512M -jar RomRaider.jar
+    goto endProcess
+:logger
+    echo starting **** RomRaider Logger ****
+    start "" "%javaDir%" -Djava.library.path=lib/windows -Dawt.useSystemAAFontSettings=lcd -Dswing.aatext=true -Dsun.java2d.d3d=true -Xms64M -Xmx512M -jar RomRaider.jar -logger
+    goto endProcess
+
+:endProcess 
+    endlocal
